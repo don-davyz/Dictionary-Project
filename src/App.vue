36541e73a5c3f3f90e30679bd3f21dfa592c041f
don-davyz <92528372+don-davyz@users.noreply.github.com>
2022-04-29 23:59:50 +0100

    more styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <form @submit.prevent="method1()">
-    <input placeholder="Enter Word" type='text' id='word' v-model="word" />
-    <button type='submit'>Meaning</button>
+  <form @submit.prevent="method1()" class="form-signin">
+    <div class="text-center mb-4">
+      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72"
+        height="72">
+      <h1 class="h3 mb-3 font-weight-normal">My Dictionary</h1>
+      <p>Build form controls with floating labels via the</p>
+    </div>
+
+    <div class="form-label-group">
+      <input type='text' id='word' v-model="word" class="form-control" placeholder="Enter word" required autofocus>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Search</button>
+    </div>
+    <div class="text-center mb-4">
+      <h1 class="h3 mb-3 font-weight-normal">Fishes</h1>
+      <p>A fish is a fish <code>:placeholder-shown</code> pseudo-element. <a
+          href="https://caniuse.com/#feat=css-placeholder-shown">Works in latest Chrome, Safari, and Firefox.</a></p>
+    </div>
+
+    
+    <p class="mt-5 mb-3 text-muted text-center">Dictionary by Davies &copy; 2022</p>
   </form>
+
 </template>
 
 <script>
@@ -20,7 +38,7 @@ export default {
   },
   methods: {
     method1: function () {
-      
+
       const word = document.getElementById('word').value;
       axios
         .get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word.value)
