3c2358ab7b3d34456777b272f36e7a12611ce304
don-davyz <92528372+don-davyz@users.noreply.github.com>
2022-04-29 23:00:03 +0100

    apis called

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
-<form @submit.prevent="getDefinitions()" >
-<input placeholder="Enter Word" type='text' id='word' v-model="word"/>
-<button type='submit'>Meaning</button>
-</form>
+  <form @submit.prevent="method1()">
+    <input placeholder="Enter Word" type='text' id='word' v-model="word" />
+    <button type='submit'>Meaning</button>
+  </form>
 </template>
 
 <script>
-import { ref } from "vue";
+import axios from 'axios';
 
 export default {
   name: 'App',
-  setup(){
-    const word = ref("");
+  mounted() {
+    this.loading = true;
+    axios
+      .get('https://random-words-api.vercel.app/word')
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+      .finally(() => this.loading = false)
+  },
+  methods: {
+    method1: function () {
+      
+      const word = document.getElementById('word').value;
+      axios
+        .get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word.value)
+        .then(response2 => console.log(response2))
+        .catch(error2 => console.log(error2))
 
-    return{ word };
+      axios
+        .get('https://api.datamuse.com/sug?s=' + word.value)
+        .then(response3 => console.log(response3))
+        .catch(error => console.log(error))
+    }
   }
- 
+
 };
+
 </script>
 
 <style>
-
 </style>
