52f0dd78712c2c2646e171dff8fdc9b403b659b4
don-davyz <92528372+don-davyz@users.noreply.github.com>
2022-04-29 21:01:35 +0100

    starting scripts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
 <form @submit.prevent="getDefinitions()" >
-<input placeholder="Enter Word" type='text' id='word'/>
+<input placeholder="Enter Word" type='text' id='word' v-model="word"/>
 <button type='submit'>Meaning</button>
 </form>
 </template>
 
 <script>
+import { ref } from "vue";
 
 export default {
   name: 'App',
+  setup(){
+    const word = ref("");
+
+    return{ word };
+  }
  
 };
 </script>
