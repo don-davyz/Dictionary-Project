7f5b6152138d2a9bd21174140940f299c06e71a8
don-davyz <92528372+don-davyz@users.noreply.github.com>
2022-04-30 17:46:12 +0100

    Multiple Api calls implementation

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,106 @@
 <template>
-  <form @submit.prevent="method1()" class="form-signin">
-    <div class="text-center mb-4">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72"
-        height="72">
-      <h1 class="h3 mb-3 font-weight-normal">My Dictionary</h1>
-      <p>Build form controls with floating labels via the</p>
-    </div>
 
+  <div class="text-center mb-4">
+    <img class="mb-4" src="logo.png" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">My Dictionary</h1>
+    <p>Definitions from Free Dictionary API · <a target="_blank" href="https://dictionaryapi.dev/">Learn more</a></p>
+  </div>
+  <form @submit.prevent="getDefinations()" class="form-signin">
     <div class="form-label-group">
-      <input type='text' id='word' v-model="word" class="form-control" placeholder="Enter word" required autofocus>
+      <input type='text' v-model="word" class="form-control" placeholder="Enter word" required autofocus>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Search</button>
     </div>
-    <div class="text-center mb-4">
-      <h1 class="h3 mb-3 font-weight-normal">Fishes</h1>
-      <p>A fish is a fish <code>:placeholder-shown</code> pseudo-element. <a
-          href="https://caniuse.com/#feat=css-placeholder-shown">Works in latest Chrome, Safari, and Firefox.</a></p>
+    <p style="color:red;" v-if="errortitle" >{{ errortitle }}: <span>{{ errormessage }}</span></p>
+  </form>
+  <div class="text-justify mt-4">
+    <div style="display:flex;">
+      <img class="m-2" src="sound.svg" alt="sound" width="30px" height="auto">
+      <h1 class="word font-weight-normal">{{ word }}</h1>
+      <div class="m-3">{{ phonetic }}</div>
     </div>
 
-    
-    <p class="mt-5 mb-3 text-muted text-center">Dictionary by Davies &copy; 2022</p>
-  </form>
+    <h6 class="mt-5">See definitions:</h6>
+
+    <div v-for="definition in definitionsArray" :key="definition">
+      <p>{{ definition }}</p>
+    </div>
+
+    <h6>Suggestion:</h6>
+
+    <span class="badge badge-default badge-outlined" v-for="suggestion in suggestionsArray" :key="suggestion">
+      {{ suggestion }}
+    </span>
+
+  </div>
+
+
+  <p class="mt-5 mb-3 text-muted text-center">Dictionary by Davies &copy; 2022</p>
+
+
 
 </template>
 
 <script>
 import axios from 'axios';
+import { ref } from "vue";
 
 export default {
   name: 'App',
+
   mounted() {
     this.loading = true;
     axios
       .get('https://random-words-api.vercel.app/word')
-      .then(response => console.log(response))
+      .then(response => {
+        this.word = response.data[0].word,
+          this.phonetic = response.data[0].pronunciation,
+          this.definitionsArray = [response.data[0].definition]
+        console.log(response)
+      })
       .catch(error => console.log(error))
       .finally(() => this.loading = false)
   },
-  methods: {
-    method1: function () {
+  setup() {
+    let errormessage, errortitle,phonetic =ref("");
+    const suggestionsArray = ref([]);
+    const definitionsArray = ref([]);
+    const word = ref("");
+    async function getDefinations() {
 
-      const word = document.getElementById('word').value;
-      axios
-        .get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word.value)
-        .then(response2 => console.log(response2))
-        .catch(error2 => console.log(error2))
+      const requestOne = axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word.value);
+      const requestTwo = axios.get('https://api.datamuse.com/sug?s=' + word.value);
+      await axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+        const responseOne = responses[0]
+        const responseTwo = responses[1]
 
-      axios
-        .get('https://api.datamuse.com/sug?s=' + word.value)
-        .then(response3 => console.log(response3))
-        .catch(error => console.log(error))
+        responseOne.data[0].meanings.forEach(({ definitions }) => {
+          definitions.forEach(({ definition }) => {
+            if (typeof definition === "string")
+              definitionsArray.value.push(definition);
+          });
+        });
+
+        responseTwo.data.forEach(({ word }) => {
+          if (typeof word === "string")
+            suggestionsArray.value.push(word);
+
+        });
+
+        this.phonetic = responseOne.data[0].phonetic;
+        
+      })).catch(errors => {
+        console.log(errors)
+        this.errortitle = errors.response.data.title,
+        this.errormessage = errors.response.data.message
+      })
+
+
     }
-  }
+
+    return { phonetic,errortitle,errormessage, suggestionsArray, definitionsArray, word, getDefinations }
+
+  },
+
 
 };
 
